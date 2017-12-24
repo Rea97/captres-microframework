53d952fc9676de9a12316fe4b5ed6d1e2a2c61e8
Oziel Martinez <ozielrea97@hotmail.com>
2017-12-24 01:55:51 -0600

    Create very basic router

---
+++ utils/Router/index.js
@@ -0,0 +1,42 @@
+let url = require('url')
+
+module.exports = class {
+    constructor() {
+        this.routes = [{method: '', path: '', action: null}]
+    }
+
+    add(path, action, method = 'GET') {
+        this.routes.push({method, path, action})
+    }
+
+    get(path, action) {
+        this.add(path, action, 'GET')
+    }
+
+    post(path, action) {
+        this.add(path, action, 'POST')
+    }
+
+    put(path, action) {
+        this.add(path, action, 'PUT')
+    }
+
+    delete(path, action) {
+        this.add(path, action, 'DELETE')
+    }
+
+    init(req, res) {
+        let path = url.parse(req.url).path
+        let method = req.method
+    
+        let route = this.routes.filter(route => route.path === path && route.method === method)
+        
+        if (route.length === 1) {
+            route[0].action(req, res)
+            return;
+        }
+
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end(`<h1>404 Route ${path} Not Found</h1>`)
+    }
+}
