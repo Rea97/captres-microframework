7121b43ae7f02d9bc46493dc8eddf863a4e0d47d
Oziel Martinez <ozielrea97@hotmail.com>
2017-12-24 01:38:09 -0600

    Create web server

---
+++ index.js
@@ -0,0 +1,10 @@
+let http = require('http');
+
+const port = process.env.PORT || 3000
+const host = process.env.host || '127.0.0.1'
+
+http.createServer((request, response) => {
+    response.end('hello')
+}).listen(port, host)
+
+console.log(`Server listening at ${port}`)
