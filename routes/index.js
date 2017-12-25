10756dcebace03cc246a83a69fc239f90ce90237
Oziel Martinez <ozielrea97@hotmail.com>
2017-12-24 18:26:52 -0600

    Clean routes file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 let Router = require('../support/Router')
 let router = new Router()
 
-router.get('/', (request, response) => {
-    response.writeHead(200, {
-        'Content-type': 'application/json'
-    })
 let WelcomeController = require('../controllers/WelcomeController')
 
-    response.end('{"message": "Hello world"}')
-})
 router.get('/', new WelcomeController().index)
 
 module.exports = router
